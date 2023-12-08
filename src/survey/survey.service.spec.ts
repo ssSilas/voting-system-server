6bc0209e84ae3cf6f36e93e3b0aaf0dddc06b1bd
Silas <guimaraessilas15@gmail.com>
2023-12-08 13:36:17 -0300

    test/refactor: find all test | create test

--- src/survey/survey.service.spec.ts
+++ src/survey/survey.service.spec.ts
@@ -1,33 +1,64 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { CreateSurveyDto, UserIdentityDTO } from './dto/survey.dto';
 import { SurveyService } from './survey.service';
+import { SurveyResponse } from './types/user.type';
+import { Test, TestingModule } from '@nestjs/testing';
 
 describe('SurveyService', () => {
   let service: SurveyService;
   let id: string;
-  let userId: string;
-  let userEmail: string;
-  let date;
+  let user: UserIdentityDTO;
+  let surveyDataResponse: SurveyResponse;
 
   beforeEach(async () => {
-    service = new SurveyService();
-    id = '3cfa4d9d-52c8-479e-ac70-87dde8ded39f';
-    userId = '5aeb0fdd-bd22-497f-a055-ea473799e95d';
-    userEmail = 'test@test.com.br';
-    date = new Date();
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-  it('should create of a course', async () => {
-    const user: UserIdentityDTO = { _id: userId, email: userEmail };
-
-    const mockSurveyRepo = {
-      create: jest.fn().mockReturnValue(Promise.resolve(true)),
+    user = {
+      _id: '5aeb0fdd-bd22-497f-a055-ea473799e95d',
+      email: 'test@test.com.br',
+    };
+    surveyDataResponse = {
+      _id: id,
+      title: 'Quanto é 2+2',
+      description: 'Some os valores, encontre os resultados e responda!',
+      options: ['1', '5', '7', '9', '0'],
+      user,
+      type: 'Geek',
+      status: 1,
     };
 
-    //@ts-expect-error defined part of methods
-    service['surveyRepo'] = mockSurveyRepo;
+    const surveyRepository = {
+      create: jest.fn().mockResolvedValue(true),
+      find: jest.fn().mockReturnThis(),
+      select: jest.fn().mockResolvedValue([surveyDataResponse]),
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        SurveyService,
+        {
+          provide: getModelToken('Survey'), // Use o token real do seu modelo
+          useValue: surveyRepository,
+        },
+      ],
+    }).compile();
+
+    service = module.get<SurveyService>(SurveyService);
+  });
+
+  it('deve criar uma instância do SurveyService', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('deve encontrar todas as enquetes', async () => {
+    const newSurvey = await service.getAll(user);
+
+    expect(service['surveyRepo'].find).toHaveBeenCalled();
+    expect(service['surveyRepo'].find().select).toHaveBeenCalledWith(
+      '_id title description options user type status',
+    );
+    expect([surveyDataResponse]).toStrictEqual(newSurvey);
+  });
+
+  it('deve criar uma enquete', async () => {
     const createSurvey: CreateSurveyDto = {
       title: 'Quanto é 2+2',
       description: 'Some os valores, encontre os resultados e responda!',
@@ -35,7 +66,11 @@ describe('SurveyService', () => {
       type: 'Geek',
     };
     const newSurvey = await service.create(user, createSurvey);
-    expect(mockSurveyRepo.create).toHaveBeenCalled();
-    expect(true).toStrictEqual(newSurvey);
+    expect(service['surveyRepo'].create).toHaveBeenCalledWith({
+      ...createSurvey,
+      user,
+    });
+    expect(service['surveyRepo'].create).toHaveBeenCalled();
+    expect(newSurvey).toBe(true);
   });
 });
